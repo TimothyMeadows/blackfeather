8f710d65dbae8d5c2dd17c5a095b330176ed0c40
Timothy Meadows II <timothy@timothymeadows.com>
2016-05-10 14:43:31 -0700

    Add extentions for compression.

---
+++ Blackfeather-Universal/Extention/BinaryCompressionExtention.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Blackfeather.Data.Compression;
+using Blackfeather.Data.Encoding;
+
+namespace Blackfeather.Extention
+{
+    public static class BinaryCompressionExtention
+    {
+        /// <summary>
+        /// Compress binary data
+        /// </summary>
+        /// <param name="value">Binary to compress</param>
+        /// <returns></returns>
+        public static byte[] Compress(this byte[] value)
+        {
+            return value == null ? null : GZipCompression.Compress(value);
+        }
+
+        /// <summary>
+        /// Decompress binary data
+        /// </summary>
+        /// <param name="value">Binary to decompress</param>
+        /// <returns></returns>
+        public static byte[] Decompress(this byte[] value)
+        {
+            return value == null ? null : GZipCompression.Decompress(value);
+        }
+    }
+}
