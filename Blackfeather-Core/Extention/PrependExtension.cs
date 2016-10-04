6d200913889b4dbd7fb425eb98377f1aaf848ba5
Timothy Meadows II <timothy@timothymeadows.com>
2016-10-03 18:57:59 -0700

    Add managed memory support. No parallels :(.

---
+++ Blackfeather-Core/Extention/PrependExtension.cs
@@ -0,0 +1,55 @@
+﻿/* 
+ The MIT License (MIT)
+
+ Copyright (c) 2013 - 2015 Timothy D Meadows II
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+using System;
+
+namespace Blackfeather.Extention
+{
+    /// <summary>
+    /// Prepend an element to an array
+    /// </summary>
+    public static class ArrayPrependExtension
+    {
+        /// <summary>
+        /// Prepend an element to an array
+        /// </summary>
+        /// <typeparam name="T">Any matching type</typeparam>
+        /// <param name="source">Source array</param>
+        /// <param name="destination">Destination array</param>
+        /// <param name="sourceLength">Length of source array, if not supplied source.Length is used.</param>
+        /// <param name="destinationLength">Length of destination array, if not supplied destination.Length is used.</param>
+        /// <returns>Transformed array</returns>
+        public static T[] Prepend<T>(this T[] source, T[] destination, int sourceLength = 0, int destinationLength = 0)
+        {
+            var expanded = new T[source.Length + destination.Length];
+            var expandSourceLength = sourceLength > 0 ? sourceLength : source.Length;
+            var expandDestinationLength = destinationLength > 0 ? destinationLength : destination.Length;
+
+            Array.Copy(destination, 0, expanded, 0, expandDestinationLength);
+            Array.Copy(source, 0, expanded, expandDestinationLength, expandSourceLength);
+
+            return expanded;
+        }
+    }
+}
---
+++ Blackfeather-Core/Extention/StringCompressionExtention.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Blackfeather.Data.Compression;
+using Blackfeather.Data.Encoding;
+
+namespace Blackfeather.Extention
+{
+    public static class StringCompressionExtention
+    {
+        /// <summary>
+        /// Compress string data using the supplied encoding
+        /// </summary>
+        /// <param name="value">String to compress</param>
+        /// <param name="encoding">Encoding to compress with</param>
+        /// <returns></returns>
+        public static string Compress(this string value, Encoding encoding = null)
+        {
+            return string.IsNullOrEmpty(value) ? string.Empty : GZipCompression.Compress(value, encoding).ToBase64();
+        }
+
+        /// <summary>
+        /// Decompress string data using the supplied encoding
+        /// </summary>
+        /// <param name="value">Base64 string to decompress</param>
+        /// <param name="encoding">Encoding to decompress with</param>
+        /// <returns></returns>
+        public static string Decompress(this string value, Encoding encoding = null)
+        {
+            return string.IsNullOrEmpty(value) ? string.Empty : GZipCompression.Decompress(value.FromBase64(), encoding);
+        }
+    }
+}
