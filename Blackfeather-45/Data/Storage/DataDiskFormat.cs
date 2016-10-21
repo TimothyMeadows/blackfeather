54338b833840c328a854611bb585bca4b0e5bc66
Timothy Meadows II <timothy@timothymeadows.com>
2016-10-20 18:16:05 -0700

    Remove DataDisk (for now).

--- Blackfeather-45/Data/Storage/DataDiskFormat.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blackfeather.Data.Storage
-{
-    public class DataDiskFormat
-    {
-        public string Label { get; set; }
-        public int Size { get; set; }
-        public bool Indexed { get; set; }
-    }
-}
